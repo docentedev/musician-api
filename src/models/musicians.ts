72c947ae304167a8eaceaecc5b3911e60f2aa767
docente.dev <docente.dev@gmail.com>
2022-06-17 01:37:12 -0400

    feat: Added description columns

--- src/models/musicians.ts
+++ src/models/musicians.ts
@@ -9,19 +9,19 @@ type GetAllPaginateProps = {
 }
 
 const musicians = (pg: pg.Pg) => {
-  const fields = ['first_name', 'last_name', 'second_last_name', 'second_name', 'birth_date', 'death_name', 'city_fk', 'alias']
+  const fields = ['first_name', 'last_name', 'second_last_name', 'second_name', 'birth_date', 'death_name', 'city_fk', 'alias', 'description']
   const qb = initQueryBuilder({
     table: 'musician',
     as: ['country as co', 'city as c', 'musician as m'],
     fields: ['id', 'first_name', 'last_name', 'country.name', 'city.name'],
     mutationFields: ['id', 'first_name', 'last_name', 'second_last_name', 'second_name', 'birth_date', 'death_date', 'city_fk', 'alias', 'city_name', 'country_name'],
-    select: 'm.id, m.first_name, m.last_name, m.second_last_name, m.second_name, m.birth_date, m.death_date, m.city_fk, m.alias, c.name as city_name, co.nicename as country_name, image',
+    select: 'm.id, m.first_name, m.last_name, m.second_last_name, m.second_name, m.birth_date, m.death_date, m.city_fk, m.alias, c.name as city_name, co.nicename as country_name, image, description',
     run: Run(pg)
   })
   const qbMutation = initQueryBuilder({
     table: 'musician',
     fields: ['id', 'first_name', 'last_name', 'country.name', 'city.name'],
-    mutationFields: ['id', 'first_name', 'last_name', 'second_last_name', 'second_name', 'birth_date', 'death_date', 'city_fk', 'alias', 'city_name', 'country_name', 'image'],
+    mutationFields: ['id', 'first_name', 'last_name', 'second_last_name', 'second_name', 'birth_date', 'death_date', 'city_fk', 'alias', 'city_name', 'country_name', 'image', 'description'],
     run: Run(pg)
   })
   const getAllPaginate = async ({ page = 1, size = 10, sort = 'desc', order = 'id' }: GetAllPaginateProps) => {
